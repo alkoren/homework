d96bb7bf37163b8ddef6274e29c8ad6665069d93
alkoren <37251680+alkoren@users.noreply.github.com>
2018-04-01 14:15:12 +0300

    Homework 5, array of fraction

--- by.epam.jb24.lab5/src/by/epam/jb24/lab5/TestFraction.java
+++ by.epam.jb24.lab5/src/by/epam/jb24/lab5/TestFraction.java
@@ -4,17 +4,25 @@ public class TestFraction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Fraction[] masFraction=new Fraction[10];
 		Fraction myFraction1 = new Fraction(3, 4);
-		Fraction myFraction2 = new Fraction();
-		Fraction myFractionRes = new Fraction();
-		myFractionRes.assignFraction(myFraction1);
+		for(int i = 0; i < 10 ; i++)
+		{
+			masFraction[i] = new Fraction(1, 2);
+		};
+		masFraction[0].assignFraction(myFraction1);
 		System.out.println("первая дробь :" + myFraction1.getFractionString());
 		System.out.println("введите вторую дробь :");	
-		myFraction2=myFraction2.enterFraction();
-		System.out.println("вторая дробь : " + myFraction2.getFractionString());
-		myFractionRes.multFraction(myFraction2);
-		System.out.println("перемножив дроби получим " + myFractionRes.getFractionString());
-		myFraction1.addFraction(myFraction2);
-		System.out.println("сложив дроби получим " + myFraction1.getFractionString());
+		masFraction[1]=masFraction[1].enterFraction();
+		System.out.println("вторая дробь : " + masFraction[1].getFractionString());
+		masFraction[2].multFraction(masFraction[1]);
+		System.out.println("перемножив дроби получим " + masFraction[2].getFractionString());
+		masFraction[3].addFraction(masFraction[2]);
+		System.out.println("сложив дроби получим " + masFraction[3].getFractionString());
+		System.out.println("Массив дробей: ");
+		for(int i = 0; i < 10 ; i++)
+		{
+			System.out.println(masFraction[i].getFractionString());
+		};
 	}
 }
